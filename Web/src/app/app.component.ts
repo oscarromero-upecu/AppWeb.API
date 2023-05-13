045c6e7078c756bb1b81fa4b73369ab5dc849682
Oscar <orrp@tecnicoecuador.edu.ec>
2023-05-13 11:08:07 -0500

    Agregar estilos al angular

--- Web/src/app/app.component.ts
+++ Web/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'Hola';
+  users: any;
+  constructor(private http: HttpClient)  {  }
+  //consumimos la api
+  ngOnInit(): void {
+    this.http.get('https://localhost:5002/api/user').subscribe({
+      next: response=> this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Conexion API Correcta')
+    })
+  }
 }
